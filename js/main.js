de6b8aa0659fa0f12eb2604312952752a99f4627
moaz ibrahem <moazibrahem952@gmail.com>
2023-01-19 10:51:53 +0200

    creat project travil

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () => { 
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+    inputNumber.oninput = () =>{
+        if(inputNumber.Value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+    };
+});
+
+
+AOS.init({
+    duration: 400,
+    delay: 200,
+ });
